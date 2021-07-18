3ff8a356aa5bffdc4249aadb1fe1b26793810bea
ar[arnau321 <arnau123@att.net>
2021-07-17 20:49:45 -0400

    Checking for bugs and updating notation Work on checking for win.

--- app/game.js
+++ app/game.js
@@ -1,41 +1,50 @@
 'use strict'
 // array of 9 for 9 squares in tic-tac-toe board
-const arrayOfBoxes = ['x', 'x', 'x', '', 'o', '', 'x', 'x', 'o']
+const gameBoard = ['x', '', 'x', 'o', 'o', 'o', '', 'x', 'o']
 
 
-// arrayOfBoxes = ['', '', '', '', '', '', '', '', '']
-const checkForWin = function (array) {
+const checkForWin = function (arrayOfBoxes) {
   // horizontal wins
   if ((arrayOfBoxes[0] === arrayOfBoxes[1] && arrayOfBoxes[1] === arrayOfBoxes[2]) &&
       (arrayOfBoxes[0] === 'x' || arrayOfBoxes[0] === 'o')) {
     console.log('you win h1')
+    return arrayOfBoxes
   } else if ((arrayOfBoxes[3] === arrayOfBoxes[4] && arrayOfBoxes[4] === arrayOfBoxes[5]) &&
       (arrayOfBoxes[3] === 'x' || arrayOfBoxes[3] === 'o')) {
     console.log('you win h2')
+    return arrayOfBoxes
   } else if ((arrayOfBoxes[6] === arrayOfBoxes[7] && arrayOfBoxes[7] === arrayOfBoxes[8]) &&
       (arrayOfBoxes[6] === 'x' || arrayOfBoxes[6] === 'o')) {
     console.log('you win h3')
+    return arrayOfBoxes
   // vertical wins
   } else if ((arrayOfBoxes[0] === arrayOfBoxes[3] && arrayOfBoxes[3] === arrayOfBoxes[6]) &&
       (arrayOfBoxes[0] === 'x' || arrayOfBoxes[0] === 'o')) {
     console.log('you win v1')
+    return arrayOfBoxes
   } else if ((arrayOfBoxes[1] === arrayOfBoxes[4] && arrayOfBoxes[4] === arrayOfBoxes[7]) &&
       (arrayOfBoxes[1] === 'x' || arrayOfBoxes[1] === 'o')) {
     console.log('you win v2')
+    return arrayOfBoxes
   } else if ((arrayOfBoxes[2] === arrayOfBoxes[5] && arrayOfBoxes[5] === arrayOfBoxes[8]) &&
       (arrayOfBoxes[0] === 'x' || arrayOfBoxes[0] === 'o')) {
     console.log('you win v3')
+    return arrayOfBoxes
   // diagonal wins
   } else if ((arrayOfBoxes[0] === arrayOfBoxes[4] && arrayOfBoxes[4] === arrayOfBoxes[8]) &&
       (arrayOfBoxes[0] === 'x' || arrayOfBoxes[0] === 'o')) {
     console.log('you win d1')
+    return arrayOfBoxes
   } else if ((arrayOfBoxes[2] === arrayOfBoxes[4] && arrayOfBoxes[4] === arrayOfBoxes[6]) &&
       (arrayOfBoxes[0] === 'x' || arrayOfBoxes[0] === 'o')) {
     console.log('you win d1')
+    return arrayOfBoxes
   } else {
-    return array
+    return arrayOfBoxes
   }
 }
 
-console.log(checkForWin(arrayOfBoxes))
+
+
+console.log(checkForWin(gameBoard))
 
